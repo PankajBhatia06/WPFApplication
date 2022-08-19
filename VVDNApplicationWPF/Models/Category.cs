edf25525d698a806f71b19c01a65c6c0420696b8
Pankaj Bhatia <pjbhatia100@gmail.com>
2022-08-19 14:27:25 +0530

    Initial commit

---
+++ VVDNApplicationWPF/Models/Category.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVDNApplicationWPF.Models
+{
+    public class Category : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Category()
+        {
+            Id = 12345;
+            Name = "Test Name";
+        }
+
+
+        private int _Id;
+        public int Id
+        {
+            get
+            {
+                return _Id;
+            }
+
+            set
+            {
+                _Id = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string _Name;
+
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                NotifyPropertyChanged();
+            }
+        }
+   
+      
+    }
+}
---
+++ VVDNApplicationWPF/Views/CategoryView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VVDNApplicationWPF.ViewModels;
+
+namespace VVDNApplicationWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for CategoryView.xaml
+    /// </summary>
+    public partial class CategoryView : Page
+    {
+        public CategoryViewModel categoryViewModel { get; set; }
+        public CategoryView()
+        {
+            InitializeComponent();
+            categoryViewModel = new CategoryViewModel();
+            this.DataContext = categoryViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"ID:{categoryViewModel.SelectedCategory.Id} and Name: {categoryViewModel.SelectedCategory.Name}", "MY WPF Application", MessageBoxButton.OK);
+            categoryViewModel.SelectedCategory.Id = int.MaxValue;
+            categoryViewModel.SelectedCategory.Name = "Again my name";
+        }
+    }
+}
