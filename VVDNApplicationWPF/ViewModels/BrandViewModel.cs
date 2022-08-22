60b3f6f7a90847c3ab11e056891e1254313c2c03
shagun pachisia <shagunpachisia07@gmail.com>
2022-08-22 14:40:53 +0530

    go

---
+++ VVDNApplicationWPF/ViewModels/BrandViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVDNApplicationWPF.Models;
+
+namespace VVDNApplicationWPF.ViewModels
+{
+    public class BrandViewModel
+    {
+        public Brands SelectedBrand { get; set; }
+
+        public BrandViewModel()
+        {
+            SelectedBrand = new Brands();
+        }
+    }
+}
