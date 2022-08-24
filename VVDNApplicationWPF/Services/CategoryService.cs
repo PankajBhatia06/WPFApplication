0d29dc5ce4897a0fd4dca03ef3720be0f9a8ec8c
Pankaj Bhatia <pjbhatia100@gmail.com>
2022-08-24 11:29:59 +0530

    MySql Initial code

---
+++ VVDNApplicationWPF/Services/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVDNApplicationWPF.Database;
+using VVDNApplicationWPF.Models;
+
+namespace VVDNApplicationWPF.Services
+{
+    public class CategoryService
+    {
+        public bool InsertCategory(Category category)
+        {
+            try
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand();
+                mySqlCommand.Connection = Connection.CreateSqlConnection();
+                mySqlCommand.CommandText = "proc_insert_category";
+                mySqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                mySqlCommand.Parameters.AddWithValue("category_name", category.Name);
+                mySqlCommand.Parameters.AddWithValue("user_id", 1);
+                mySqlCommand.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- VVDNApplicationWPF/ViewModels/CategoryViewModel.cs
+++ VVDNApplicationWPF/ViewModels/CategoryViewModel.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VVDNApplicationWPF.Models;
+using VVDNApplicationWPF.Services;
 
 namespace VVDNApplicationWPF.ViewModels
 {
     public class CategoryViewModel
     {
+        private readonly CategoryService categoryService;
         public Category SelectedCategory { get; set; }
 
         public CategoryViewModel()
         {
             SelectedCategory = new Category();
+            categoryService = new CategoryService();
+        }
+
+        public bool SaveCategory()
+        {
+            return categoryService.InsertCategory(SelectedCategory);
         }
     }
 }
